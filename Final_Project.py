aeaec4e7038cfb3c230e2f419eec6efd4e6d9c48
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-03-07 09:56:30 -0500

    start making the welcome code

---
+++ Final_Project.py
@@ -0,0 +1,9 @@
+# My text based game for my Final Project
+# Kaden Seeger
+
+
+
+def welcome:
+    print('Hello Welcome! to my game where all of the options look like this, Do you understand?')
+    print ('Yes        No')
+    print ((':'),)
