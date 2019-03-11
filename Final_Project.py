eb3aff3b571f1899d45a7f0f74d6e82df1be9242
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-03-11 09:05:59 -0400

    Add welcome screen

--- Final_Project.py
+++ Final_Project.py
@@ -1,9 +1,22 @@
 # My text based game for my Final Project
 # Kaden Seeger
 
+Name = input('What is your name: ')
+def welcome():
+    try:
+        choice = int(input('Your Answer: '))
+        if choice < 1.1:
+            print("")
+            print ('Next path(1)')
+        else:
+            print("")
+            print ('other path(2)')
+    except ValueError:
+        print("")
+        print('Thats not an option')
 
-
-def welcome:
-    print('Hello Welcome! to my game where all of the options look like this, Do you understand?')
-    print ('Yes        No')
-    print ((':'),)
+print('Hello and welcome to my game where all of the options look like this, do you understand?: ')
+print('Yes(1)            No(2)')
+welcome()
+print("")
+# the welcome screen
