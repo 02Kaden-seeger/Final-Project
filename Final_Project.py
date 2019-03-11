352cd071883bc2bb8896e95b199071693dbb9f36
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-03-11 09:55:47 -0400

    Made the beginning of the game

--- Final_Project.py
+++ Final_Project.py
@@ -2,15 +2,47 @@
 # Kaden Seeger
 
 Name = input('What is your name: ')
+
+def pathbloodcurdalscreamyes():
+    print("As you travel towards the scream you see a old house that was not on any map you checked before you  ")
+    try:
+        choice = int(input('Your Answer: '))
+        if choice < 1.1:
+            print("")
+            print ('Next path(1(1)')
+        else:
+            print("")
+            print ('other path(2(1)')
+    except ValueError:
+        print("")
+        print('Thats not an option')
+
+def start():
+    print("You are in a dark forest, the last thing you remember is you planning a trip in the forest with your buddy and his girlfriend.")
+    print(" You hear a blood curdling scream, do you go towards it ?")
+    print('Yes(1)            No(2)')
+    try:
+        choice = int(input('Your Answer: '))
+        if choice < 1.1:
+            print("")
+            print ('Next path(1(1)')
+        else:
+            print("")
+            print ('other path(2(1)')
+    except ValueError:
+        print("")
+        print('Thats not an option')
+
+
 def welcome():
     try:
         choice = int(input('Your Answer: '))
         if choice < 1.1:
             print("")
-            print ('Next path(1)')
+            print (start())
         else:
             print("")
-            print ('other path(2)')
+            print (welcome())
     except ValueError:
         print("")
         print('Thats not an option')
@@ -20,3 +52,4 @@ print('Yes(1)            No(2)')
 welcome()
 print("")
 # the welcome screen
+
