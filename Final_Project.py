1a48fcc9b513160166882f4f7d06e023276cd97e
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-03-12 09:57:59 -0400

    Add more options the buddy and added more plot

--- Final_Project.py
+++ Final_Project.py
@@ -1,10 +1,47 @@
 # My text based game for my Final Project
 # Kaden Seeger
 
-Name = input('What is your name: ')
 
-def pathbloodcurdalscreamyes():
-    print("As you travel towards the scream you see a old house that was not on any map you checked before you  ")
+name = input('What is your name: ')
+
+def start2():
+    print ('You go to him and he says', name, 'th..th..they took her' )
+    print('he points where the scream came from')
+    print ('do you go to the scream?')
+    print('Yes(1)            No(2)')
+    try:
+        choice = int(input('Your Answer: '))
+        if choice < 1.1:
+            print("")
+            print(start1())
+        else:
+            print("")
+            print('other path(2(1)')
+    except ValueError:
+        print("")
+        print('Thats not an option')
+
+def item1():
+    print ('before you go to the scream there is a flashlight')
+    print('do you take it?')
+    print('Yes(1)            No(2)')
+    try:
+        choice = int(input('Your Answer: '))
+        if choice < 1.1:
+            print("")
+            print (start1())
+        else:
+            print("")
+            print (start2())
+    except ValueError:
+        print("")
+        print('Thats not an option')
+
+def start1():
+    print("As you travel towards the scream you see a old house that was not on any map you checked before you planed the trip")
+    print ('when you get closer you see a dark figure running away from the house screaming YOUR NEXT')
+    print ('do you go into the house or go where the dark figure went?')
+    print ('House(1)            Dark figure(2)')
     try:
         choice = int(input('Your Answer: '))
         if choice < 1.1:
@@ -18,20 +55,23 @@ def pathbloodcurdalscreamyes():
         print('Thats not an option')
 
 def start():
-    print("You are in a dark forest, the last thing you remember is you planning a trip in the forest with your buddy and his girlfriend.")
-    print(" You hear a blood curdling scream, do you go towards it ?")
-    print('Yes(1)            No(2)')
+    print('You, your buddy and his girlfriend are camping in the woods when you get there it is pretty late so you decide to go to bed in the tent')
+    print('')
+    print("You wake up in the dark forest you see your friend bleeding out on the ground")
+    print("You hear a blood curdling scream, do you go towards it or go to your friend?")
+    print('Scream(1)            Friend(2)')
     try:
         choice = int(input('Your Answer: '))
         if choice < 1.1:
             print("")
-            print ('Next path(1(1)')
+            print (start1())
         else:
             print("")
-            print ('other path(2(1)')
+            print (start2())
     except ValueError:
         print("")
         print('Thats not an option')
+        start2()
 
 
 def welcome():
